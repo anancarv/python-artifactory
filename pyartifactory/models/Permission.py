0d22e7207155ac62560f943addc8ba77746e79d5
Ananias CARVALHO <ananias.carvalho@ext.adeo.com>
2019-04-11 11:42:50 +0200

    Add permissions integration

--- pyartifactory/models/Permission.py
+++ pyartifactory/models/Permission.py
@@ -1,6 +1,30 @@
+from enum import Enum
+from typing import List, Dict, Optional
+
 from pydantic import BaseModel
 
 
+class SimplePermission(BaseModel):
+    name: str
+    uri: str
+
+
+class PermissionEnum(str, Enum):
+    admin = "m"
+    delete = "d"
+    deploy = "w"
+    annotate = "n"
+    read = "r"
+
+
+class PrincipalsPermission(BaseModel):
+    users: Optional[Dict[str, List[PermissionEnum]]] = None
+    groups = Optional[Dict[str, List[PermissionEnum]]] = None
+
+
 class Permission(BaseModel):
-    # ToDo
-    pass
+    name: str
+    includesPattern: str = "**"
+    excludesPattern: str = ""
+    repositories: List[str]
+    principals: PrincipalsPermission
