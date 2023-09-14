a5c70a70579196c5ea8074c0083c4d4a234ba221
Helio Chissini de Castro <heliocastro@gmail.com>
2023-09-14 07:52:41 +0200

    refactor(init): Add proper object export style
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- pyartifactory/__init__.py
+++ pyartifactory/__init__.py
@@ -1,16 +1,37 @@
+# Copyright (c) 2019 Ananias
+# Copyright (c) 2023 Helio Chissini de Castro
+#
+# Licensed under the MIT license: https://opensource.org/licenses/MIT
+# Permission is granted to use, copy, modify, and redistribute the work.
+# Full license information available in the project LICENSE file.
+#
+# SPDX-License-Identifier: MIT
+
 """
 Import all object definitions here.
 """
+from __future__ import annotations
+
 from pyartifactory.objects import (
-    ArtifactoryUser,
-    ArtifactoryGroup,
-    ArtifactorySecurity,
-    ArtifactoryRepository,
-    ArtifactoryArtifact,
-    ArtifactoryPermission,
-    Artifactory,
     AccessTokenModel,
+    Artifactory,
+    ArtifactoryArtifact,
+    ArtifactoryGroup,
+    ArtifactoryPermission,
+    ArtifactoryRepository,
+    ArtifactorySecurity,
+    ArtifactoryUser,
 )
 
+__all__ = [
+    "AccessTokenModel",
+    "Artifactory",
+    "ArtifactoryGroup",
+    "ArtifactoryArtifact",
+    "ArtifactoryPermission",
+    "ArtifactoryRepository",
+    "ArtifactorySecurity",
+    "ArtifactoryUser",
+]
 
 __version__ = "1.13.0"
--- pyartifactory/models/__init__.py
+++ pyartifactory/models/__init__.py
@@ -1,34 +1,72 @@
+# Copyright (c) 2019 Ananias
+# Copyright (c) 2023 Helio Chissini de Castro
+#
+# Licensed under the MIT license: https://opensource.org/licenses/MIT
+# Permission is granted to use, copy, modify, and redistribute the work.
+# Full license information available in the project LICENSE file.
+#
+# SPDX-License-Identifier: MIT
+
 """
 Import all models here.
 """
-from typing import Union, Type
-
-from .auth import AuthModel, ApiKeyModel, PasswordModel, AccessTokenModel
-from .group import Group, SimpleGroup
-from .user import NewUser, UserResponse, BaseUserModel, SimpleUser, User
-from .repository import (
-    LocalRepository,
-    LocalRepositoryResponse,
-    VirtualRepository,
-    VirtualRepositoryResponse,
-    RemoteRepository,
-    RemoteRepositoryResponse,
-    SimpleRepository,
-)
+from __future__ import annotations
 
 from .artifact import (
-    ArtifactPropertiesResponse,
-    ArtifactStatsResponse,
     ArtifactFileInfoResponse,
     ArtifactFolderInfoResponse,
     ArtifactInfoResponse,
+    ArtifactPropertiesResponse,
+    ArtifactStatsResponse,
 )
+from .auth import AccessTokenModel, ApiKeyModel, AuthModel, PasswordModel
+from .group import Group, SimpleGroup
 from .permission import Permission, PermissionV2, SimplePermission
+from .repository import (
+    LocalRepository,
+    LocalRepositoryResponse,
+    RemoteRepository,
+    RemoteRepositoryResponse,
+    SimpleRepository,
+    VirtualRepository,
+    VirtualRepositoryResponse,
+)
+from .user import BaseUserModel, NewUser, SimpleUser, User, UserResponse
 
+AnyRepositoryResponse = LocalRepositoryResponse | VirtualRepositoryResponse | RemoteRepositoryResponse
 
-AnyRepositoryResponse = Union[
-    LocalRepositoryResponse, VirtualRepositoryResponse, RemoteRepositoryResponse
+AnyRepository = LocalRepository | VirtualRepository | RemoteRepository
+AnyPermission = Permission | PermissionV2
+
+__all__ = [
+    "ArtifactFileInfoResponse",
+    "ArtifactFolderInfoResponse",
+    "ArtifactInfoResponse",
+    "ArtifactPropertiesResponse",
+    "ArtifactStatsResponse",
+    "AccessTokenModel",
+    "ApiKeyModel",
+    "AuthModel",
+    "PasswordModel",
+    "Group",
+    "SimpleGroup",
+    "Permission",
+    "PermissionV2",
+    "SimplePermission",
+    "LocalRepository",
+    "LocalRepositoryResponse",
+    "RemoteRepository",
+    "RemoteRepositoryResponse",
+    "SimpleRepository",
+    "VirtualRepository",
+    "VirtualRepositoryResponse",
+    "User",
+    "UserResponse",
+    "BaseUserModel",
+    "NewUser",
+    "SimpleUser",
+    "AnyRepositoryResponse",
+    "AnyRepository",
+    "AnyPermission",
+    "Artifact",
 ]
-
-AnyRepository = Union[LocalRepository, VirtualRepository, RemoteRepository]
-AnyPermission = Union[Permission, PermissionV2]
