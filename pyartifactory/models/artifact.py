df8423066c4ae7d16b078f4da3710ef8834499e5
sigma67 <ytmusicapi@gmail.com>
2023-10-05 17:53:05 +0200

    remove license headers

--- pyartifactory/models/artifact.py
+++ pyartifactory/models/artifact.py
@@ -1,12 +1,3 @@
-# Copyright (c) 2019 Ananias
-# Copyright (c) 2023 Helio Chissini de Castro
-#
-# Licensed under the MIT license: https://opensource.org/licenses/MIT
-# Permission is granted to use, copy, modify, and redistribute the work.
-# Full license information available in the project LICENSE file.
-#
-# SPDX-License-Identifier: MIT
-
 """
 Definition of all artifact models.
 """
